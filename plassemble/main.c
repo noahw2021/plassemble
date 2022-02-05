7b536e23e12852852363750f0e69a3e3dabbfaec
Noah Wooten <noah.wooten@icloud.com>
2022-02-05 13:29:38 -0500

    add skeleton

--- plassemble/main.c
+++ plassemble/main.c
@@ -6,9 +6,32 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
-	printf("Hello, World!\n");
-	return 0;
+#define ArgumentBody(Source, Name) if (!strcmp(Source, Name))
+int main(int argc, char** argv) {
+	if (argc < 2) {
+		printf("Plassemble: PLASM assembler.\n");
+		printf("%s -a (outfile): Start assembler in interactive mode.\n", argv[0]);
+		printf("%s -f (outfile) (infile): Start assembler with file.\n", argv[0]);
+		printf("%s -h: Show this screen.\n", argv[0]);
+		printf("%s -s: Display syntax.\n", argv[0]);
+	}
+	
+	char* Source = argv[1];
+	ArgumentBody(Source, "-a") {
+		
+	}
+	
+	ArgumentBody(Source, "-f") {
+		
+	}
+	
+	ArgumentBody(Source, "-h") {
+		
+	}
+	
+	ArgumentBody(Source, "-s") {
+		
+	}
 }
