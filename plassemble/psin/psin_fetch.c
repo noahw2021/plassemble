bd1cf4abf4a948c0518559a2668563c229eec170
Noah Wooten <noah.wooten@icloud.com>
2022-02-07 11:05:58 -0500

    fetch

---
+++ plassemble/psin/psin_fetch.c
@@ -0,0 +1,31 @@
+//
+//  psin_fetch.c
+//  plassemble
+//
+//  Created by Noah Wooten on 2/7/22.
+//
+
+#include "psin.h"
+#include <stdlib.h>
+
+u32 psin_getbyopcode(byte Opcode) {
+	for (int i = 0; i < InstructionCount; i++) {
+		if (InstructionMap[i].Opcode == Opcode)
+			return i;
+	}
+	return 0xFFFFFFFF;
+}
+u32 psin_getbymnemonic(char* Mnemonic) {
+	char* Uppercased = malloc(strlen(Mnemonic) + 1);
+	rtl_strupr(Mnemonic, Uppercased);
+	
+	for (int i = 0; i < InstructionCount; i++) {
+		if (!strcmp(InstructionMap[i].Mnemonic, Uppercased)) {
+			free(Uppercased);
+			return i;
+		}
+	}
+	
+	free(Uppercased);
+	return 0xFFFFFFFF;
+}
---
+++ plassemble/psin/psin_getdata.c
@@ -0,0 +1,8 @@
+//
+//  psin_getdata.c
+//  plassemble
+//
+//  Created by Noah Wooten on 2/7/22.
+//
+
+#include <stdio.h>
