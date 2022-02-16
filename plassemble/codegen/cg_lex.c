14a6ae3dcf0dcabb6b12cc7dfa899ab71ff4e702
Noah Wooten <noah.wooten@icloud.com>
2022-02-16 10:32:33 -0500

    use psin for instructions

--- plassemble/codegen/cg_lex.c
+++ plassemble/codegen/cg_lex.c
@@ -7,6 +7,7 @@
 
 #include "codegen.h"
 #include "ops.h"
+#include "../psin/psin.h"
 #include <stdlib.h>
 #include <string.h>
 u32 ErrorLevel;
@@ -54,99 +55,13 @@ void cgi_lexicalparse(char* OperationStr, char* OperandAStr, char* OperandBStr, 
 	rtl_strupr(OperandAStr, SourceOperandA);
 	rtl_strupr(OperandBStr, SourceOperandB);
 	rtl_strupr(OperandCStr, SourceOperandC);
-	Instruction(SourceOperation, Operation, SET)
-	Instruction(SourceOperation, Operation, LDWM)
-	Instruction(SourceOperation, Operation, STWM)
-	Instruction(SourceOperation, Operation, LDBM)
-	Instruction(SourceOperation, Operation, STBM)
-	Instruction(SourceOperation, Operation, OR)
-	Instruction(SourceOperation, Operation, AND)
-	Instruction(SourceOperation, Operation, XOR)
-	Instruction(SourceOperation, Operation, NOT)
-	Instruction(SourceOperation, Operation, ADD)
-	Instruction(SourceOperation, Operation, SUB)
-	Instruction(SourceOperation, Operation, MUL)
-	Instruction(SourceOperation, Operation, DIV)
-	Instruction(SourceOperation, Operation, SETI)
-	Instruction(SourceOperation, Operation, LDWMI)
-	Instruction(SourceOperation, Operation, STWMI)
-	Instruction(SourceOperation, Operation, LDBMI)
-	Instruction(SourceOperation, Operation, STBMI)
-	Instruction(SourceOperation, Operation, ORI)
-	Instruction(SourceOperation, Operation, ANDI)
-	Instruction(SourceOperation, Operation, XORI)
-	Instruction(SourceOperation, Operation, ADDI)
-	Instruction(SourceOperation, Operation, SUBI)
-	Instruction(SourceOperation, Operation, MULI)
-	Instruction(SourceOperation, Operation, DIVI)
-	Instruction(SourceOperation, Operation, SL)
-	Instruction(SourceOperation, Operation, SR)
-	Instruction(SourceOperation, Operation, SLI)
-	Instruction(SourceOperation, Operation, SRI)
-	Instruction(SourceOperation, Operation, SA)
-	Instruction(SourceOperation, Operation, SP)
-	Instruction(SourceOperation, Operation, SAI)
-	Instruction(SourceOperation, Operation, GO)
-	Instruction(SourceOperation, Operation, ENT)
-	Instruction(SourceOperation, Operation, RT)
-	Instruction(SourceOperation, Operation, RTF)
-	Instruction(SourceOperation, Operation, RTFI)
-	Instruction(SourceOperation, Operation, GOI)
-	Instruction(SourceOperation, Operation, ENTI)
-	Instruction(SourceOperation, Operation, CMP)
-	Instruction(SourceOperation, Operation, CMPI)
-	Instruction(SourceOperation, Operation, GOF)
-	Instruction(SourceOperation, Operation, ENTF)
-	Instruction(SourceOperation, Operation, ENTFI)
-	Instruction(SourceOperation, Operation, ENTFII)
-	Instruction(SourceOperation, Operation, GOFI)
-	Instruction(SourceOperation, Operation, GOFII)
-	Instruction(SourceOperation, Operation, NEG)
-	Instruction(SourceOperation, Operation, EIH)
-	Instruction(SourceOperation, Operation, DIH)
-	Instruction(SourceOperation, Operation, HLT)
-	Instruction(SourceOperation, Operation, INT)
-	Instruction(SourceOperation, Operation, INTI)
-	Instruction(SourceOperation, Operation, HND)
-	Instruction(SourceOperation, Operation, HNDI)
-	Instruction(SourceOperation, Operation, HNDII)
-	Instruction(SourceOperation, Operation, SPA)
-	Instruction(SourceOperation, Operation, SPG)
-	Instruction(SourceOperation, Operation, SAA)
-	Instruction(SourceOperation, Operation, SAG)
-	Instruction(SourceOperation, Operation, LDSD)
-	Instruction(SourceOperation, Operation, LDSDI)
-	Instruction(SourceOperation, Operation, STSD)
-	Instruction(SourceOperation, Operation, LDVM)
-	Instruction(SourceOperation, Operation, LDVMI)
-	Instruction(SourceOperation, Operation, STVM)
-	Instruction(SourceOperation, Operation, SPN)
-	Instruction(SourceOperation, Operation, MSP)
-	Instruction(SourceOperation, Operation, SSP)
-	Instruction(SourceOperation, Operation, ASP)
-	Instruction(SourceOperation, Operation, SPB)
-	Instruction(SourceOperation, Operation, SAB)
-	Instruction(SourceOperation, Operation, SAII)
-	Instruction(SourceOperation, Operation, INC)
-	Instruction(SourceOperation, Operation, DEC)
-	Instruction(SourceOperation, Operation, SBN)
-	Instruction(SourceOperation, Operation, SBN)
-	Instruction(SourceOperation, Operation, IOB)
-	Instruction(SourceOperation, Operation, IIB)
-	Instruction(SourceOperation, Operation, IOW)
-	Instruction(SourceOperation, Operation, IIW)
-	Instruction(SourceOperation, Operation, IOS)
-	Instruction(SourceOperation, Operation, IIS)
-	Instruction(SourceOperation, Operation, LDQM)
-	Instruction(SourceOperation, Operation, LDQMI)
-	Instruction(SourceOperation, Operation, LDHM)
-	Instruction(SourceOperation, Operation, LDHMI)
-	Instruction(SourceOperation, Operation, STQM)
-	Instruction(SourceOperation, Operation, STQMI)
-	Instruction(SourceOperation, Operation, STHM)
-	Instruction(SourceOperation, Operation, STHMI) { // End of else
+	
+	u32 InstructionLexicalIterator = psin_getbymnemonic(SourceOperation);
+	if (InstructionLexicalIterator == 0xFFFFFFFF) {
 		ErrorLevel++;
 		cgs_posterror(_CGS_INVALIDOP, SourceOperation, CurrentLine);
+	} else {
+		*Operation = psin_getopcode(InstructionLexicalIterator);
 	}
 	
 	if (SourceOperandA[0]	 != '\0') {
